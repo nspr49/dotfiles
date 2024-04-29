bc6a1f996c53d6177a1919c03857997ad1ab312d
Noah Springer <extra@Noah-PC>
2024-04-29 15:46:27 +0200

    treesitter svelte

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return{
     config = function ()
     local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "java", "css", "angular", "python","typescript","json" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "java", "css", "angular", "python","typescript","json", "svelte", "javascript" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = false },  
