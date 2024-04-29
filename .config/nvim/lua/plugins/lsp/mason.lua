6c7f36b01c9f36c9868fcb256a700325dd653a73
Noah Springer <extra@Noah-PC>
2024-04-29 21:46:38 +0200

    updated jdtls

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,16 +1,40 @@
 return{
   "williamboman/mason.nvim",
   dependencies = {
-	 "williamboman/mason-lspconfig.nvim",
---"neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    --"neovim/nvim-lspconfig",
   },
   config = function()
-  require("mason").setup()
-  require("mason-lspconfig").setup(
-		{
-		 ensure_installed = {"lua_ls", "clangd","angularls", "tsserver",--[["jdtls", "java_language_server"  --]]},
-		  automatic_installation = false,
-		}
+    require("mason").setup()
+    require("mason-lspconfig").setup(
+    {
+      ensure_installed = {"html","cssls", "lua_ls", "clangd","angularls", "tsserver","jdtls"--[[ "java_language_server"  --]]},
+      automatic_installation = false,
+    })
+    require('mason-tool-installer').setup({
+      ensure_installed={
+        'java-debug-adapter',
+        'java-test',
+        }
+    })
+    local lspconfig = require('lspconfig')
+    local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local lsp_attach = function(client,bufnr)
+    end
+    
 
-	 )
-  end,}
+    require("mason-lspconfig").setup_handlers({
+      function (server_name)
+        --dont call setup for jdtls
+        if server_name ~= 'jdtls' then
+          lspconfig[server_name].setup({
+            on_attach = lsp_attach,
+            capabilities = lsp_capabilities,
+          })
+          end
+        end
+          }) 
+        end
+        } 
+        
--- .config/nvim/lua/plugins/mcfusseneger-jdtls.lua
+++ .config/nvim/lua/plugins/mcfusseneger-jdtls.lua
@@ -1,4 +1,5 @@
 return{
 'mfussenegger/nvim-jdtls',
-
+--only open in java file
+  ft = 'java',
 }
