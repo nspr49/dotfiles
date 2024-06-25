d4edcb191b100956258bcae19349d975c0e24f79
noah springer <springers.noah@gmail.com>
2024-06-25 09:13:21 +0200

    telescope show hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,15 @@
 return {
 		"nvim-telescope/telescope.nvim", tag = '0.1.5',
 		dependencies = {'nvim-lua/plenary.nvim',
-							 "sharkdp/fd",}
+							 "sharkdp/fd",},
+    config = function ()
+      local telescope = require("telescope")
+      telescope.setup {
+        pickers = {
+          find_files= {
+            hidden=true
+          }
+        }
+      }
+    end
 }
