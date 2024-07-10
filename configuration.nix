1c0a8db027fde82056221b95f5faa616ce747ec0
Noah Springer <noah.soringer@mni.thm.de>
2024-07-10 15:02:28 +0200

    hilfe

---
+++ configuration.nix
@@ -0,0 +1,169 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+    /etc/nixos/hardware-configuration.nix
+    ];
+
+# Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+# Enable networking
+#networking.wireless.iwd.enable = true;
+    networking.networkmanager.enable = true;
+#networking.networkmanager.wifi.backend = "iwd";
+#networking.wireless.enable = true; 
+# Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+# Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+# Configure keymap in X11
+  services.xserver = {
+    enable=true;
+    layout = "de";
+    xkbVariant = "";
+    displayManager.gdm = {
+      enable = true;
+      wayland=true;
+    };
+  };
+
+# Configure console keymap
+  console.keyMap = "de";
+
+#enable zsh
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+#starship
+  programs.starship = {
+    enable = true;
+  };
+
+#enable pulseaudio
+  hardware.pulseaudio.enable=true;
+
+
+#nix.settings.experimental-features = ["nix-command" "flakes"];
+# Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.extra = {
+    isNormalUser = true;
+    description = "extra";
+    extraGroups = [ "networkmanager" "wheel" "audio" ];
+    packages = with pkgs; [];
+  };
+
+# Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs.hyprland.enable=true;
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim 
+      wget
+      pkgs.libappindicator-gtk3
+      pkgs.neovim
+      pkgs.wlroots
+      grim
+      pkgs.dconf
+      pkgs.gtk3
+      kitty
+      pkgs.polkit
+      xwayland
+      slurp
+      wl-clipboard
+      mako
+      git
+      firefox
+      pkgs.alacritty
+      stow
+      pkgs.waybar
+      pkgs.nodejs_22
+      home-manager
+      
+#wm
+      pkgs.hyprland
+#pkgs.hyprpaper # fuck this
+# sway bg
+      pkgs.swaybg
+#menu 
+      pkgs.wofi
+      rofi-wayland
+      pkgs.pipewire
+#network
+      pkgs.networkmanager
+#pkgs.wg-netmanager
+      pkgs.networkmanagerapplet
+      pkgs.cacert
+#pyenv
+      pkgs.pyenv
+      pkgs.python3
+      pkgs.python312Packages.cmake
+#zsh
+      pkgs.zsh
+      pkgs.starship
+      #c
+      gcc
+      gdb
+      gnumake
+      ];
+
+  fonts.packages = with pkgs; [
+    nerdfonts
+
+  ];
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
