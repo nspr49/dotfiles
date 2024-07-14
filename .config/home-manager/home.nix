1da0e414e028ab5cf5fa9537b7b71da6e7fbd6c3
Noah Springer <noah.soringer@mni.thm.de>
2024-07-14 21:28:17 +0200

    pushed secrets fml

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -34,13 +34,19 @@
   neovim
   tmux
   firefox
+  fastfetch
   spotify
+  #notification deamon
+  dunst
   ];
 
   programs.tmux = {
     enable = true;
 
   };
+
+
+
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
