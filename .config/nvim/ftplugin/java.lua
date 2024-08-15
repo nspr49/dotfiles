354d2a15c3edeb01e940be5ad7fd105b5c217c5a
noah springer <springers.noah@gmail.com>
2024-08-15 17:36:37 +0200

    added formatting

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -9,6 +9,26 @@ local config = {
           }
         }
       }
-    }
+    },
 }
+--[[
+augroup_id = vim.api.nvim_create_augroup(
+"FormatModificationsDocumentFormattingGroup",
+{ clear = false }
+)
+vim.api.nvim_clear_autocmds({ group = augroup_id, buffer = vim.api.nvim_get_current_buf() })
+
+vim.api.nvim_create_autocmd(
+{ "BufWritePre" },
+{
+  group = augroup_id,
+  buffer = vim.api.nvim_get_current_buf(),
+  callback = function()
+    local lsp_format_modifications = require"lsp-format-modifications"
+    lsp_format_modifications.format_modifications(vim.lsp.get_clients({ name = "jdtls", bufnr = buf })[1], vim.api.nvim_get_current_buf())
+  end,
+}
+)
+]]--
+vim.keymap.set('v', '<Leader>1f', vim.lsp.buf.format, bufopts)
 require('jdtls').start_or_attach(config)
