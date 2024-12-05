908fc4fd0cff377241b20aded565e66f96a24fe1
Noah Springer <noah.soringer@mni.thm.de>
2024-12-05 14:22:42 +0100

    bla

--- nix/laptop/flake.nix
+++ nix/laptop/flake.nix
@@ -1,30 +1,60 @@
 {
+  description = "A very basic flake";
+
   inputs = {
+   zjstatus = {
+      url = "github:dj95/zjstatus";
+    };
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: 
+  outputs = inputs@{ self, nixpkgs, home-manager,zjstatus, ... }: 
     let
-      lib = nixpkgs.lib;
-      in {
-        nixosConfigurations = {
-          nixos = lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [ 
-            ./configuration.nix 
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
+    lib = nixpkgs.lib;
 
-             home-manager.users.extra = import ./home.nix;
-            }
+    pkgs = import nixpkgs {
+  system = "x86_64-linux";
+  overlays = [
+    (final: prev: {
+      zjstatus = inputs.zjstatus.packages."x86_64-linux".default;
+      hyprpanel = inputs.hyprpanel.packages."x86_64-linux".default;
+    })
+  ];
+config = {
+allowUnfree = true;
+};
+};
+
+
+  in {
+#    environment.systemPackages = [ inputs.wezterm.packages.${pkgs.system}.default ];
+pkgs."x86_64-linux".default = [
+pkgs.zjstatus
+pkgs.hyprpanel
+];
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        modules = [ 
+          ./configuration.nix 
+          home-manager.nixosModules.home-manager
+          {
+
+            home-manager.extraSpecialArgs = {
+              inherit inputs pkgs;
+            };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.extra = import ./home.nix;
+          }
             ];
           };
         };
       };
+
 }
