096aa51541bfab727892f7b26a674a3e30da4f18
nspringer <nspringer@wave-distribution.de>
2025-01-20 16:39:18 +0100

    fix

--- .config/nvim/lua/plugins/lsp/blink.lua
+++ .config/nvim/lua/plugins/lsp/blink.lua
@@ -46,7 +46,7 @@ return {
       ['<C-k>'] = { 'select_prev', 'fallback' },
       ['<C-n>'] = { 'select_next', 'fallback' },
       ['<C-j>'] = { 'select_next', 'fallback' },
-      ['<CR>'] = {'accept', 'fallback'},
+      --['<CR>'] = {'accept', 'fallback'},
 
       -- disable a keymap from the preset
 
@@ -102,13 +102,13 @@ return {
            }
           },
 
-        
     },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'otter', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      --[[
       providers = {
         lsp = {
           score_offset = 1000,
@@ -124,6 +124,7 @@ return {
         },
 
       },
+      --]]
     },
   },
 
