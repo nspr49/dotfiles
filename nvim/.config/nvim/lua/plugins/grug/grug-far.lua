373ea7b58ac91ab6f74a7ff8de53a04240516d86
Noah Springer <nspringer@wave-distribution.de>
2025-02-25 14:01:03 +0100

    grug change

--- nvim/.config/nvim/lua/plugins/grug/grug-far.lua
+++ nvim/.config/nvim/lua/plugins/grug/grug-far.lua
@@ -16,14 +16,14 @@ return {
 
     vim.keymap.set("n", "<leader>gf", function() require('grug-far').open({}) end,
       { silent = true, desc = "Replace in Path" });
-    vim.keymap.set({ "v", "n" }, "<leader>gv", function() require('grug-far').with_visual_selection() end,
+    vim.keymap.set({ "v"}, "<leader>gf", function() require('grug-far').with_visual_selection() end,
       { silent = true, desc = "Replace visual in path" });
 
-    vim.keymap.set({ "v", "n" }, "<leader>gcf",
+    vim.keymap.set({ "n" }, "<leader>gc",
       function() require('grug-far').open({ prefills = { paths = vim.fn.expand("%") } }) end,
       { silent = true, desc = "Replace in current file" });
 
-    vim.keymap.set({ "v", "n" }, "<leader>gcv",
+    vim.keymap.set({ "v" }, "<leader>gc",
       function() require('grug-far').with_visual_selection({ prefills = { paths = vim.fn.expand("%") } }) end,
       { silent = true, desc = "Replace visual in current file" });
   end
